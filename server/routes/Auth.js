d87f4e46c389310a820566fed05542b0066c4ac7
mohammed5420 <almoaz065@gmail.com>
2021-12-01 00:02:02 -0800

    <Backend> Create api Routes

---
+++ server/routes/Auth.js
@@ -0,0 +1,50 @@
+/**
+ * ### Auth Route
+ * #### 1- Signup
+ * #### 2- Login
+ * #### 3- Logout
+ * #### 4- Forget Password
+ * #### 5- Reset Password
+ */
+
+const routes = require("express").Router();
+
+/**
+ * 3- Signup user
+ */
+routes.post("/signup", (req,res) => {
+
+});
+
+/**
+ * Login user
+ */
+
+routes.post("/login", (req,res) => {
+
+});
+
+/**
+ * Logout user
+ */
+
+routes.get("/logout", (req,res) => {
+
+});
+
+/**
+ * forget-password 
+ */
+
+routes.post("/forget-password", (req,res) => {
+
+});
+
+/**
+ * Reset user password
+ */
+
+routes.post("/reset-password", (req,res) => {
+
+});
+
---
+++ server/routes/Cards.js
@@ -0,0 +1,42 @@
+/**
+ * ### Flash Cards Routes
+ * #### 1- Get All flash cards
+ * #### 2- Create new flash card
+ * #### 3- Update flash card by card ID
+ * #### 4- Delete flash card by card ID
+ */
+
+const routes = require("express").Router();
+
+/**
+ * 1- Fetch All user Flash cards
+ */
+routes.get("/",(req,res) => {
+
+});
+
+/**
+ * 2- Create new flash card
+ */
+
+ routes.post("/create",(req,res) => {
+
+});
+
+/**
+ * 3- Update flash card by card ID
+ */
+
+routes.patch("/update/:card_id",(req,res) => {
+
+});
+
+/**
+ * 4- Remove flash card by ID
+ */
+
+routes.delete("/delete/:card_id",(req,res) => {
+
+});
+
+
