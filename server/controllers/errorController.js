cc0ad63a7e37ed7db4d19588c7e987e995e1ce0a
mosalah <almoaz065@gmail.com>
2022-01-25 21:14:59 +0200

    test: 💍 hange error message based on the environment
    ✅ Closes: #1

--- server/controllers/errorController.js
+++ server/controllers/errorController.js
@@ -1,8 +1,31 @@
-module.exports = (error, req, res, next) => {
-  error.statusCode = error.statusCode || 500;
-  error.status = error.status || "Error";
-  res.status(error.statusCode).json({
+const handleProdErrors = (error,res) => {
+  if (error.isOperational) {
+    res.status(error.statusCode).json({
+      status: error.status,
+      message: error.message,
+    });
+  } else {
+    res.status(500).json({
+      status: "error",
+      message: "something went wrong!",
+    });
+  }
+}
+
+const handleDevErrors = (error,res) => {
+  res.status(error.status).json({
     status: error.status,
     message: error.message,
+    stack: error.stack,
   });
+}
+module.exports = (error, req, res, next) => {
+  error.statusCode = error.statusCode || 500;
+  error.status = error.status ||"Error";
+
+  if (process.env.ENVIRONMENT === "development") {
+    handleDevErrors(error,res);
+  } else if(process.env.ENVIRONMENT === "prduction") {
+    handleProdErrors(error,res);
+  }
 };
