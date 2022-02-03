29118d646f4cca2f053bd63756e42a662e22a35f
mosalah <almoaz065@gmail.com>
2022-02-03 07:19:45 +0200

    test: 💍 Handle Games Routes
    added QA Game routes and QA Game controller

--- server/controllers/gamesController.js
+++ server/controllers/gamesController.js
@@ -1,1 +1,34 @@
+const catchAsync = require("../utils/catchAsync");
+
 //TODO: Flash-Card Games controller 
+
+/**
+ * Serve random cards
+ */
+exports.getGameCards = catchAsync(async (req,res,next) => {
+
+})
+
+/**
+ * Save game history
+ */
+
+exports.saveGameStats = catchAsync(async (req,res,next) => {
+
+});
+
+/**
+ * Update game history
+ */
+
+ exports.updateGameStats = catchAsync(async (req,res,next) => {
+
+});
+
+/**
+ * Delete Game history
+ */
+
+ exports.deleteGameStats = catchAsync(async (req,res,next) => {
+
+});
--- server/routes/Games.js
+++ server/routes/Games.js
@@ -1,1 +1,11 @@
+const { getGameCards, saveGameStats, updateGameStats, deleteGameStats } = require("../controllers/gamesController");
+
 //TODO: Flash-Cards Games Routes
+const router = require("express").Router();
+
+//Main Route flash-cards/games
+router
+    .get("/qa",getGameCards)
+    .post("/qa", saveGameStats)
+    .patch("/qa", updateGameStats)
+    .delete("/qa", deleteGameStats)
