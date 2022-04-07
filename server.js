fb5f062c452769278bf8efab0cc3625d8845307d
mosalah <almoaz065@gmail.com>
2022-04-07 13:36:48 +0200

    feat: 🎸 Implementing rate limit protection to api routes

--- server.js
+++ server.js
@@ -9,8 +9,15 @@ require("dotenv").config();
 const AppError = require("./utils/errorsHandler");
 const globalHandler = require("./controllers/errorController");
 const userAgent = require("./middleware/userAgent");
+const rateLimit = require("express-rate-limit");
 
+const limiter = rateLimit({
+  max: 150,
+  windowMs: 60 * 60 * 1000,
+  message: "too many requests from this ip please try again later",
+});
 //Middleware
+app.use("/api", limiter);
 app.use(express.json());
 app.use(
   express.urlencoded({
